506f6f8bd45e49711558660a88574acebd11a402
Kirill Yackubovskiy <92030020+Vasilich563@users.noreply.github.com>
2024-09-20 12:29:58 +0300

    Create snippet_bounds.py

---
+++ src/sync/snippet_bounds.py
@@ -0,0 +1,43 @@
+# Author: Vodohleb04
+class SnipetBounds:
+    """
+    Dataclass to save snippet bounds.
+
+    snippet_start - index from the original text, that corresponds to the first char of the snippet
+    snippet_end - index from the original text, that corresponds to the after-last char of the snippet
+
+    snippet = original_text[snippet_start : snippet_end]  (original_text[snippet_end] is excluded from the snippet)
+    """
+
+    def __init__(self, snippet_start, snippet_end):
+        self._snippet_start = snippet_start
+        self._snippet_end = snippet_end
+
+    
+    @property
+    def snippet_start(self) -> int:
+        """
+        snippet = original_text[snippet_start : snippet_end]  (original_text[snippet_end] is excluded from the snippet)
+
+        returns: int - index from the original text, that corresponds to the first char of the snippet
+        """
+        return self._snippet_start
+    
+
+    @property
+    def snippet_end(self) -> int:
+        """
+        snippet = original_text[snippet_start : snippet_end]  (original_text[snippet_end] is excluded from the snippet)
+
+        returns: int - index from the original text, that corresponds to the after-last char of the snippet
+        """
+        return self._snippet_end
+
+
+    def __str__(self):
+        return f"SnippetBounds({self._snippet_start}, {self._snippet_end})"
+    
+
+    def __repr__(self):
+        return f"SnippetBounds(snippet_start: {self._snippet_start}, snippet_end: {self._snippet_end})"
+
